7f9d7ca0cbe706ecdaeda36b2321f3d20166f5b8
juchkajuli <60781573+juchkajuli@users.noreply.github.com>
2020-05-20 15:25:46 +0400

    Add files via upload

---
+++ src/components/modal-window.vue
@@ -0,0 +1,117 @@
+<template>
+    <div >
+        <transition name="modal">
+            <div class="modal-mask">
+                <div class="modal-wrapper">
+                    <div class="modal-container">
+
+                        <div class="modal-header">
+                            <slot name="header">
+                                Настройки
+                            </slot>
+                        </div>
+
+                        <div class="modal-body">
+                            <slot name="body">
+                                Сообщить о проблеме
+                            </slot>
+                        </div>
+
+                        <div class="modal-footer">
+                            <slot name="footer">
+                                <router-link to="/input" class="input">
+                                Выйти из аккуанта </router-link>
+                                <button class="modal-default-button" @click="$emit('close')">
+                                    Отмена
+                                </button>
+                            </slot>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "WindowModal",
+        data: function () {
+            return {
+                showModal: false
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 20px;
+        left: 35%;
+        width: 100px;
+        height: 200px;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: flex;
+        transition: opacity 0.3s ease;
+    }
+
+    .modal-wrapper {
+        display: flex;
+        vertical-align: middle;
+    }
+
+    .modal-container {
+        width: 300px;
+        margin: 0px auto;
+        padding: 20px 30px;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+        transition: all 0.3s ease;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+
+    .modal-header h3 {
+        margin-top: 0;
+        color: #42b983;
+    }
+
+    .modal-body {
+        margin: 20px 0;
+    }
+
+    .modal-default-button {
+        float: right;
+    }
+
+    /*
+     * The following styles are auto-applied to elements with
+     * transition="modal" when their visibility is toggled
+     * by Vue.js.
+     *
+     * You can easily play with the modal transition by editing
+     * these styles.
+     */
+
+
+    .modal-enter {
+        opacity: 0;
+    }
+
+    .modal-leave-active {
+        opacity: 0;
+    }
+
+    .modal-enter .modal-container,
+    .modal-leave-active .modal-container {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
+.input {
+    text-decoration: none;
+    color: inherit;
+}
+</style>
