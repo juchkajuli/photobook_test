da93aa8e57149293e49bfe975972bd40f16eb05c
Юлия <60781573+juchkajuli@users.noreply.github.com>
2020-07-03 16:27:02 +0400

    Add files via upload

--- src/views/Input.vue
+++ src/views/Input.vue
@@ -17,14 +17,19 @@
                         <div class="_logo_">
                             <span class="logotype_name"> Photobook </span>
                         </div>
-                            <form class="form_">
+                            <form class="form_" @submit="checkLogin" novalidate="true" action="Account.vue">
+                                <p v-if="errors.length">
+                                    <b>Пожалуйста исправьте указанные ошибки:</b>
+                                <ul>
+                                    <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+                                </ul>
+                                </p>
                               <div class="login">
                                   <div class="login-field">
                                       <label class="field_log">
-                                          
                                           <input aria-label="Номер телефона или Логин" aria-required="true"
                                           maxlength="75" name="username" type="text" class="input-login" value=""
-                                          placeholder="Номер телефона или Логин">   
+                                          placeholder="Номер телефона или Логин" v-model="login">
                                       </label>
                                   </div>
                               </div>
@@ -32,14 +37,14 @@
                                   <div class="password_">
                                       <label class="inp_passw">
                                           <input class="password_log" aria-label="Пароль" aria-required="true"
-                                          name="password" type="password" value="" placeholder="Пароль">
+                                          name="password" type="password" value="" placeholder="Пароль" v-model="password">
                                       </label>
                                   </div>
                               </div>
                               <div class="but_inp">
-                                  <button class="button_input" type="button">
-                                      <span class="input_but"> Войти </span>
-                                  </button>
+                                  <input type="submit" class="button_input" value="Войти">
+
+
                               </div>
                               <div class="mem_passw">
                                   <a href="#" class="password_memory"> Забыли пароль? </a>
@@ -66,6 +71,32 @@ export default {
     name: "Input",
     components: {
         FooterComponent
+    },
+    data: function () {
+        return {
+            errors:[],
+            login: null,
+            password: null
+        }
+    },
+    methods: {
+   checkLogin: function (e) {
+       if (this.login && this.password) {
+           return true;
+       }
+       this.errors = [];
+       if (!this.login) {
+           this.errors.push('Требуется указать номер телефона или Логин');
+       }
+       if (!this.password) {
+           this.errors.push('Требуетcя указать пароль');
+       }
+       e.preventDefault();
+   },
+        validLogin: function (login) {
+            var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(login);
+        }
     }
 }
 </script>
